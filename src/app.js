bc50151a7ded67e500055f0c6d76f1fb3ad3d261
AlbertPitram <albertvidallopez02@gmail.com>
2025-03-29 19:38:34 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+function generateExcuse() {
+  let who = ['The dog', 'Joe Biden', 'Ted Kazinsky', 'Pedro Sanchez'];
+  let action = ['ate', 'destroyed', 'robbed', 'set in fire'];
+  let what = ['my homework', 'my Jack Daniels bottle', 'the ta paper'];
+  let when = ['before the class', 'when I was playing Balatro', 'while I was driving like Ryan Gosling', 'during my exercise rutine', 'while I was eating a durum kebab'];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let excuse = randomWho + ' ' + randomAction + ' ' + randomWhat + ' ' + randomWhen + '.';
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ 
+  document.getElementById('excuse').innerHTML = excuse;
+}
